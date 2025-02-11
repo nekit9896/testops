24ba27945d8a329aa2b065be5d5a199d0db661dd
Никита Владимиров <nekit9896@gmail.com>
2025-02-11 11:07:11 +0300

    minio start + web + get reports

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 import os
 from datetime import datetime
 
-from flask import Blueprint, jsonify, request
+from flask import (Blueprint, jsonify, render_template, request,
+                   send_from_directory)
 from werkzeug.utils import secure_filename
 
 from app import db
 from app.models import TestRun
-from constants import UPLOAD_FOLDER
-from helpers import allowed_file
+from constants import ALLURE_REPORT_NAME, UPLOAD_FOLDER
+from helpers import allowed_file, create_reports_list, get_report
 
 bp = Blueprint("routes", __name__)
 
@@ -48,3 +49,72 @@ def upload_results():
     db.session.commit()
 
     return jsonify({"run_id": test_run.id, "message": "Файлы успешно загружены"}), 201
+
+
+@bp.route(rule="/", methods=["GET"])
+@bp.route(rule="/index", methods=["GET"])
+@bp.route(rule="/index/", methods=["GET"])
+def home():
+    """
+    Домашняя страница
+    """
+    try:
+        return render_template("index.html"), 200
+
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
+
+
+@bp.route(rule="/reports", methods=["GET"])
+def get_reports():
+    """
+    Возвращает страницу со списком отчетов
+    """
+    # Получает имена директорий с отчетами
+    result_names = create_reports_list()
+    reports = []
+    # Для каждого прогона заполняем словарь
+    if result_names:
+        for name in result_names:
+            # Здесь требуется подключение к базе и данные о прогоне будем подтягивать из базы
+            reports.append(
+                {
+                    "id": 0,
+                    "name": name,
+                    "date": datetime.now().isoformat(),
+                    "status": "success",
+                }
+            )
+
+        return (
+            render_template(template_name_or_list="reports.html", reports=reports),
+            200,
+        )
+    # Если отсутствуют результаты прогонов, то будет выведен пустой список отчетов
+    if not result_names:
+        return (
+            render_template(template_name_or_list="reports.html", reports=reports),
+            200,
+        )
+
+    else:
+        return jsonify({"error": "Ошибка при получении списка отчетов"}), 500
+
+
+@bp.route(rule="/reports/<int:result_id>", methods=["GET"])
+def view_report(result_id: int):
+    """
+    Открывает отчет
+    """
+    # Получает название директории с отчетом по id
+    result_dir_name = str(result_id)
+    # Получает директорию, в которой находится отчет
+    report_dir = get_report(result_dir_name)
+    try:
+        return send_from_directory(report_dir, ALLURE_REPORT_NAME), 200
+
+    except FileNotFoundError:
+        return jsonify({"error": "Нет такого отчета"}), 404
+
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
--- helpers.py
+++ helpers.py
@@ -1,5 +1,120 @@
-from constants import ALLOWED_EXTENSIONS
+import os
+import subprocess
+
+from werkzeug.utils import secure_filename
+
+from app import db
+from app.models import TestRun
+from constants import (ALLOWED_EXTENSIONS, ALLURE_REPORT_FOLDER_NAME,
+                       ALLURE_REPORT_NAME, ALLURE_RESULT_FOLDER_NAME)
 
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def valid_files(files):
+    """Проверка на наличие и валидность файлов."""
+    return files and not all(f.filename == "" for f in files)
+
+
+def save_files(run_name, files):
+    """Сохраняет файлы в базе данных."""
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_content = file.read()
+
+            test_run = TestRun(
+                run_name=run_name, file_name=filename, file_content=file_content
+            )
+            db.session.add(test_run)
+
+    db.session.commit()
+
+
+def get_project_subdir_path(folder_name: str) -> str:
+    """
+    Получает путь к папке в корне проекта
+    """
+    return os.path.join(os.getcwd(), folder_name)
+
+
+def check_report_exist(report_dir: str) -> bool:
+    """
+    Проверяет наличие отчета.
+    """
+    report_path = os.path.join(report_dir, ALLURE_REPORT_NAME)
+    return os.path.isfile(report_path)
+
+
+def get_folder_names(directory: str) -> list[str]:
+    """
+    Возвращает список названий папок в указанной директории.
+    """
+    return [
+        name
+        for name in os.listdir(directory)
+        if os.path.isdir(os.path.join(directory, name))
+    ]
+
+
+def create_reports_list():
+    """Функция получения списка отчетов."""
+    # Получает путь к директории с отчетами
+    results_folder_path = get_project_subdir_path(ALLURE_RESULT_FOLDER_NAME)
+    # Получает имена директорий с отчетами
+    result_names = get_folder_names(results_folder_path)
+    return result_names
+
+
+def generate_report(result_dir_path: str, report_dir_path: str) -> None:
+    """
+    Создает html-версию отчета командой в консоли.
+    """
+    command = [
+        "allure",
+        "generate",
+        result_dir_path,
+        "-o",
+        report_dir_path,
+        "--clean",
+        "-c",
+        "--single-file",
+    ]
+    try:
+        subprocess.run(command, shell=True, text=True, check=True)
+
+    except subprocess.CalledProcessError as error:
+        raise RuntimeError(
+            f"Ошибка при генерации Allure-отчета: Код {error.returncode}\n"
+            f"Вывод ошибки: {error.stderr.strip() if error.stderr else 'Нет вывода ошибки'}"
+        )
+
+
+def get_report(result_dir_name: str) -> str:
+    """
+    Получает директорию, в которой находится html отчет
+    """
+    # Получает путь к папке с результатами прогонов
+    results_dir_path = get_project_subdir_path(ALLURE_RESULT_FOLDER_NAME)
+    # Получает путь к папке с отчетами
+    reports_dir_path = get_project_subdir_path(ALLURE_REPORT_FOLDER_NAME)
+    # Получает путь к папке с результатами конкретного прогона
+    result_dir_path = os.path.join(results_dir_path, result_dir_name)
+    # Получает путь к папке с html отчетом
+    report_dir_path = os.path.join(reports_dir_path, result_dir_name)
+
+    # Проверяет наличие отчета
+    if check_report_exist(report_dir_path):
+        return report_dir_path
+
+    else:
+        # Создает html версию отчета командой в консоли
+        generate_report(result_dir_path, report_dir_path)
+
+        if check_report_exist(report_dir_path):
+            return report_dir_path
+
+        else:
+            raise FileNotFoundError("Ошибка при получении html файла с отчетом")
